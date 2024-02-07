47193060f40d05b239c917982b4be46d6e54b99e
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-02-07 04:48:19 +0300

    refactor: Decompose `match` arms into functions

--- src/cli.rs
+++ src/cli.rs
@@ -2,24 +2,22 @@ use clap::{Parser, ValueEnum};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 pub enum Query {
-    /// The workspace that is currently focused
+    /// The workspace that is currently focused.
     ActiveWorkspace,
-    /// All workspaces as an array
+    /// All workspaces as an array.
     Workspaces,
-    /// The currently focused window
+    /// The currently focused window.
     ActiveWindow,
-    /// The current keyboard layout
+    /// The current keyboard layout.
     KeyboardLayout,
 }
 
 #[derive(Parser, Debug)]
-// #[commmand(author, version, about, long_about = None)]
 pub struct Args {
-    /// What information to query Hyprland for
+    /// What information to query Hyprland for.
     #[arg(short, long, value_enum)]
     pub query: Query,
-
-    /// Wait for corresponding events and re-query when they happen
+    /// Wait for corresponding events and re-query when they happen.
     #[arg(short, long)]
     pub subscribe: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,130 @@
-use clap::Parser;
-use color_eyre::Result;
-use hyprland::{
-    data::Workspace,
-    event_listener::EventListenerMutable,
-    shared::{Address, HyprData, HyprDataActive, HyprDataActiveOptional},
-};
+//! # Tiny tool for getting information from Hyprland's IPC
 
 mod cli;
 
-use cli::Args;
-use cli::Query;
+use crate::cli::{Args, Query};
+use clap::Parser;
+use color_eyre::eyre::Result;
+use hyprland::{
+    data::{Client, Workspace},
+    event_listener::EventListenerMutable,
+    shared::{Address, HyprData, HyprDataActive, HyprDataActiveOptional},
+};
 
 fn main() -> Result<()> {
     color_eyre::install()?;
     let args = Args::parse();
 
     match args.query {
-        Query::ActiveWorkspace => {
-            let workspace = hyprland::data::Workspace::get_active()?;
-            println!("{}", workspace.id);
-            if args.subscribe {
-                let mut event_listener = EventListenerMutable::new();
-                event_listener.add_workspace_change_handler(|ws, _| println!("{ws}"));
-                event_listener.start_listener()?;
-            }
-        }
-        Query::Workspaces => {
-            let handler = || {
-                let workspaces = hyprland::data::Workspaces::get().unwrap();
-                let mut workspaces = workspaces.collect::<Vec<Workspace>>();
-                for id in 1..=10 {
-                    if !workspaces.iter().any(|ws| ws.id == id) {
-                        let monitor = workspaces.get(0).unwrap().monitor.clone();
-                        workspaces.push(Workspace {
-                            id,
-                            name: id.to_string(),
-                            monitor,
-                            windows: 0,
-                            fullscreen: false,
-                            last_window: Address::new(String::new()),
-                            last_window_title: String::new(),
-                        });
-                    }
-                }
-                workspaces.sort_unstable_by_key(|ws| ws.id);
-                let workspaces_json = serde_json::to_string(&workspaces).unwrap();
-                println!("{workspaces_json}");
-            };
+        Query::ActiveWorkspace => handle_active_workspace(&args)?,
+        Query::Workspaces => handle_workspaces(&args)?,
+        Query::ActiveWindow => handle_active_window(&args)?,
+        Query::KeyboardLayout => handle_keyboard_layout(&args)?,
+    }
 
-            handler();
-            if args.subscribe {
-                // TODO: Refactor this horrible shit
-                let mut event_listener = EventListenerMutable::new();
-                event_listener.add_workspace_added_handler(move |_, _| handler());
-                event_listener.add_workspace_destroy_handler(move |_, _| handler());
-                event_listener.add_workspace_moved_handler(move |_, _| handler());
-                event_listener.add_window_open_handler(move |_, _| handler());
-                event_listener.add_window_close_handler(move |_, _| handler());
-                event_listener.add_window_moved_handler(move |_, _| handler());
-                event_listener.add_window_moved_handler(move |_, _| handler());
-                event_listener.start_listener()?;
-            }
-        }
-        Query::ActiveWindow => {
-            let handler = || {
-                if let Some(window) =
-                    // BUG: always panicks for some reason
-                    hyprland::data::Client::get_active()
-                        .expect("could not get active window")
-                {
-                    let active_window_json = serde_json::to_string(&window).unwrap();
-                    println!("{active_window_json}");
-                }
-            };
+    Ok(())
+}
 
-            handler();
-            if args.subscribe {
-                let mut event_listener = EventListenerMutable::new();
-                event_listener.add_active_window_change_handler(move |_, _| handler());
-                event_listener.start_listener()?;
-            }
-        }
-        Query::KeyboardLayout => {
-            let printer = |layout: &str| {
-                println!(
-                    "{}",
-                    match layout {
-                        "English (US)" => "English",
-                        _ => layout,
-                    }
-                )
-            };
-            let keyboards = hyprland::data::Devices::get()?.keyboards;
-            if let Some(laptop_kb) = keyboards
-                .iter()
-                .find(|kb| kb.name == "at-translated-set-2-keyboard")
-            {
-                printer(&laptop_kb.active_keymap);
-            }
+fn handle_active_workspace(args: &Args) -> Result<()> {
+    let workspace = hyprland::data::Workspace::get_active()?;
+    println!("{}", workspace.id);
+    if args.subscribe {
+        let mut listener = EventListenerMutable::new();
+        listener.add_workspace_change_handler(|ws, _| println!("{ws}"));
+        listener.add_active_monitor_change_handler(|ws, _| println!("{}", ws.workspace));
+        listener.start_listener()?;
+    }
+    Ok(())
+}
 
-            if args.subscribe {
-                let mut event_listener = EventListenerMutable::new();
-                event_listener.add_keyboard_layout_change_handler(move |event, _| {
-                    if let Some(layout) = event.keyboard_name.split(',').nth(1) {
-                        printer(layout);
-                    }
+fn handle_workspaces(args: &Args) -> Result<()> {
+    let handler = || {
+        let workspaces = hyprland::data::Workspaces::get().unwrap();
+        let mut workspaces = workspaces.collect::<Vec<Workspace>>();
+        for id in 1..=10 {
+            if !workspaces.iter().any(|ws| ws.id == id) {
+                let monitor = workspaces.first().unwrap().monitor.clone();
+                workspaces.push(Workspace {
+                    id,
+                    name: id.to_string(),
+                    monitor,
+                    windows: 0,
+                    fullscreen: false,
+                    last_window: Address::new(String::new()),
+                    last_window_title: String::new(),
                 });
-                event_listener.start_listener()?;
             }
         }
+        workspaces.sort_unstable_by_key(|ws| ws.id);
+        let workspaces_json = serde_json::to_string(&workspaces).unwrap();
+        println!("{workspaces_json}");
+    };
+
+    handler();
+    if args.subscribe {
+        // FIXME: Refactor this horrible shit.
+        let mut listener = EventListenerMutable::new();
+        listener.add_workspace_added_handler(move |_, _| handler());
+        listener.add_workspace_destroy_handler(move |_, _| handler());
+        listener.add_workspace_moved_handler(move |_, _| handler());
+        listener.add_window_open_handler(move |_, _| handler());
+        listener.add_window_close_handler(move |_, _| handler());
+        listener.add_window_moved_handler(move |_, _| handler());
+        listener.add_window_moved_handler(move |_, _| handler());
+        listener.start_listener()?;
+    }
+
+    Ok(())
+}
+
+fn handle_active_window(args: &Args) -> Result<()> {
+    let printer = || -> Result<()> {
+        if let Some(window) = Client::get_active()? {
+            let active_window_json = serde_json::to_string(&window)?;
+            println!("{active_window_json}");
+        }
+        Ok(())
+    };
+
+    printer()?;
+    if args.subscribe {
+        let mut listener = EventListenerMutable::new();
+        listener.add_active_window_change_handler(move |_, _| {
+            let _ = printer();
+        });
+        listener.start_listener()?;
+    }
+
+    Ok(())
+}
+
+fn handle_keyboard_layout(args: &Args) -> Result<()> {
+    let printer = |layout: &str| {
+        println!(
+            "{}",
+            match layout {
+                "English (US)" => "English",
+                _ => layout,
+            }
+        );
+    };
+
+    let keyboards = hyprland::data::Devices::get()?.keyboards;
+    if let Some(laptop_kb) = keyboards
+        .iter()
+        .find(|kb| kb.name == "at-translated-set-2-keyboard")
+    {
+        printer(&laptop_kb.active_keymap);
+    }
+
+    if args.subscribe {
+        let mut listener = EventListenerMutable::new();
+        listener.add_keyboard_layout_change_handler(move |event, _| {
+            if let Some(layout) = event.keyboard_name.split(',').nth(1) {
+                printer(layout);
+            }
+        });
+        listener.start_listener()?;
     }
 
     Ok(())
